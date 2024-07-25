76bddded827de37027dd681baba2ea9f03007ca1
FR-Amira <frih.amira1995@gmail.com>
2024-07-25 10:59:46 +0200

    .

---
+++ FristP.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+void main(){
+    int x;
+    float y;
+
+    printf("donne les valeur x et y : ");
+    scanf("%d %f",&x,&y);
+    printf("valeur x= %d et valeur y= %.2f \n",x,y);
+
+
+}

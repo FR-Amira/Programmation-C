08145bbad204b2f728eff35922ed6587810260ba
FR-Amira <frih.amira1995@gmail.com>
2024-07-26 01:35:45 +0200

    Boucle

---
+++ Boucle.c
@@ -0,0 +1,36 @@
+  #include<stdio.h>
+
+void main(){
+
+int x=5;
+////////// while  si condition faux n'est excute pas 0 fois
+while (x<5)
+{
+    printf("%d\n",x);
+    x++;
+}
+
+
+
+///// do while il execute au moins  fois  c a d il excute experstion dand do puis test condition 
+
+do
+{
+     printf("la valeur de x= %d\n",x);
+    x++;
+}
+    
+while (x<5);
+
+/// FOR   intialise variable dans la boucle for 
+//  si condition faux n'est excute pas 0 fois
+
+for (int y = 5; y < 5; y++)
+{
+    printf("la valeur de y= %d\n",y);
+ 
+}
+
+}
+  
+

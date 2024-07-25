d05794904537093d71c22fa0a7aee13f9fa17a53
FR-Amira <frih.amira1995@gmail.com>
2024-07-26 01:01:16 +0200

    Ternay Operateur

--- FristP.c
+++ FristP.c
@@ -3,10 +3,28 @@
 void main(){
     int x;
     float y;
+    int z;
+    float r;
 
-    printf("donne les valeur x et y : ");
-    scanf("%d %f",&x,&y);
-    printf("valeur x= %d et valeur y= %.2f \n",x,y);
+    printf("donne les valeur x , y et z : ");
+    scanf("%d %f %d",&x,&y,&z);
+    printf("valeur x= %d , valeur y= %.2f et valuer z= %d \n",x,y,z);
 
+   /*
+    if(x==z){
+        r=15.00;
+        printf("r=%.2f",r);
+    }
+    else {
+        r=20.22;
+          printf("r=%.2f",r);
+    }
+    */
 
+// Ternay Operateur 
+      
+      r= (x==y) ? 15.00 : 20.22 ;
+    
+     printf("r=%.2f",r);
+    
 }
